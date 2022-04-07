9bd4a46b63b4063c129e1f41691612ef1d5d2bb5
BaneJil <100521725+Z-BaneJil@users.noreply.github.com>
2022-04-07 23:58:40 +0300

    2.3.1

--- src/ru/BaneJil/Bot/Config/BotConfig.java
+++ src/ru/BaneJil/Bot/Config/BotConfig.java
@@ -1,6 +1,6 @@
 package ru.BaneJil.Bot.Config;
 
 public class BotConfig {
-    public static String botToken = "OTQ5OTk2Njc2NjQ5NzIxODk3.YiSfWQ.ENXMlgNosW4F_44T6eSjZmMnQ7A";
+    public static String botToken = "";
     public static String botPrefix = "/";
 }
